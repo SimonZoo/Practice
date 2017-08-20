f633c18234fb6af1bf7110511b75e02c5f322304
SimonZoo <simonzoo@yeah.net>
2017-08-20 20:19:31 +0800

    Modified AJAX.js

--- AJAX/AJAX.js
+++ AJAX/AJAX.js
@@ -1,6 +1,7 @@
 function AJAX(url,fnSuccess, fnError) {
 	var theAJAX = XMLHttpRequest();
 	theAJAX.open('GET', url, true);//open(方法，文件名，异步传输)
+	//url + 't = ' + new Date().getTime()  时间戳阻止缓存，通过每一次改变url实现
 	theAJAX.send();//发送请求
 	//接收返回
 	theAJAX.onreadystatechange = function(){
