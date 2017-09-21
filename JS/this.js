6bc343c74bd21580b1214baa63ce60480d0ef9b9
SimonZoo <simonzoo@yeah.net>
2017-09-21 21:35:22 +0800

    Add this.js

---
+++ JS/this.js
@@ -0,0 +1,60 @@
+//默认绑定
+
+var func = function () {
+    var x = 10;
+    console.log('this in func: ' + this);//这里的this是指向global的
+    return this.x;
+}
+console.log(func());
+//undefined,因为此时this指向的是全局变量global 
+//无法改变global的值，给他加一个x: 9的属性，所以global没有x，返回undefined
+
+
+//再看一个例子
+
+var name = 'String global';
+function outFunction() {
+    console.log('in outFunction - this.name : ' + this.name);
+    name = 'Simon'; //给这句加上this的话就是创建了一个对象，该对象的name = Simon 在node会输出Simon
+
+    function inFunction() {
+        console.log('is inFunction : ' + this.name);
+    }   
+    inFunction();
+}
+outFunction();
+
+//在浏览器下
+//thisTest()中的this仍旧绑定到window对象，所以输出‘String global’， 下一个输出Simon,因为name的值给更新了
+//而node环境下用上this之后都是undefine，如果不用this，那就也是变量和重新赋值了
+
+
+
+
+
+//隐性绑定
+function say () {
+    console.log(this.name);
+}
+var objA = {
+    name : 'objA',
+    say : say
+}
+objA.say();
+
+
+
+//隐性绑定丢失上下文 
+var name_2 = 'name_2';
+function say_2 () {
+    console.log(this.name_2);
+}
+var objC = {
+    name_2 : 'objC',
+    say : say_2
+}
+var alias = objC.say_2;
+alias();
+//浏览器下输出的是name_2 而不是要像隐性绑定的结果那样会输出objC
+//因为在 alias = objC.say_2 = say_2 的传递中，函数传递并不是传递值
+//而是引用传递，所以等同于alias = say_2 ，this仍旧为windo全局对象
