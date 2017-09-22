dd85c0b76e18f954f17f1055908a6fce16680c40
SimonZoo <simonzoo@yeah.net>
2017-09-22 21:30:19 +0800

    Add this2.js

--- JS/this.js
+++ JS/this.js
@@ -1,5 +1,4 @@
 //默认绑定
-
 var func = function () {
     var x = 10;
     console.log('this in func: ' + this);//这里的this是指向global的
@@ -11,7 +10,6 @@ console.log(func());
 
 
 //再看一个例子
-
 var name = 'String global';
 function outFunction() {
     console.log('in outFunction - this.name : ' + this.name);
@@ -23,7 +21,6 @@ function outFunction() {
     inFunction();
 }
 outFunction();
-
 //在浏览器下
 //thisTest()中的this仍旧绑定到window对象，所以输出‘String global’， 下一个输出Simon,因为name的值给更新了
 //而node环境下用上this之后都是undefine，如果不用this，那就也是变量和重新赋值了
@@ -43,7 +40,6 @@ var objA = {
 objA.say();
 
 
-
 //隐性绑定丢失上下文 
 var name_2 = 'name_2';
 function say_2 () {
@@ -51,10 +47,70 @@ function say_2 () {
 }
 var objC = {
     name_2 : 'objC',
-    say : say_2
+    say_2 : say_2
 }
 var alias = objC.say_2;
 alias();
 //浏览器下输出的是name_2 而不是要像隐性绑定的结果那样会输出objC
 //因为在 alias = objC.say_2 = say_2 的传递中，函数传递并不是传递值
 //而是引用传递，所以等同于alias = say_2 ，this仍旧为windo全局对象
+
+
+
+
+//显示绑定 apply，call，bind
+var name_3 = 'global name';
+function speak() {
+    console.log(this.name_3);
+}
+var objD = {
+    name_3 : 'in objD'
+}
+var objE = {
+    name_3 : 'in objE'
+}
+
+speak();//'global name'
+speak.call(objD);//'in objD'
+speak.apply(objE);//'in objE'
+//call，apply用法相似，区别在于传递参数时apply以数组形式传递
+//call 依次给出就好
+
+function count(num1,num2) {
+    console.log(this.a + num1 + num2);
+}
+var objF = {
+    a : 3
+}
+var countA = count.bind(objF);
+countA(1,2);
+
+var countB = count.bind(objF, 4);
+countB(7);
+
+
+
+//new 绑定 es6
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        console.log('name : ' + name + '\n age : ' + age);
+    }
+}
+var simon = new Person('Simon', 20);
+simon;
+
+//另一个new绑定的例子
+function Student (number, age) {
+    this.number = number;
+    this.age = age;
+    console.log('Student :' + number + '\n age : ' + age + this);
+}
+
+Student(1001,20);
+var jack = new Student(1002, 11);
+
+
+
+
